674f2ea0a60576698ce45fd0ed7fb71b51a2f24a
DaiChiora <111251517+DaiChiora@users.noreply.github.com>
2023-04-21 13:12:24 -0300

    cors

--- src/main/java/com/ap/ap/controller/ExperienciaController.java
+++ src/main/java/com/ap/ap/controller/ExperienciaController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/experiencia")
-@CrossOrigin(origins ="*")
 
 public class ExperienciaController {
     @Autowired
