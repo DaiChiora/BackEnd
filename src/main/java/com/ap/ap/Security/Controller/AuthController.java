4fabfcb7efba8a2de95efc2ecebbf3a603a93d87
DaiChiora <111251517+DaiChiora@users.noreply.github.com>
2023-04-21 19:03:59 -0300

    corrijo auth

--- src/main/java/com/ap/ap/Security/Controller/AuthController.java
+++ src/main/java/com/ap/ap/Security/Controller/AuthController.java
@@ -30,24 +30,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "https://portfolio-daichiora-front.web.app/")
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins ="**")
+
 public class AuthController {
     @Autowired
-    @Lazy
     PasswordEncoder passwordEncoder;
     @Autowired
-    @Lazy
     AuthenticationManager authenticationManager;
     @Autowired
-    @Lazy
     UsuarioServiceSecurity usuarioServiceSecurity;
     @Autowired
-    @Lazy
     RolService rolService;
     @Autowired
-    @Lazy
     JwtProvider jwtProvider;
     
     
