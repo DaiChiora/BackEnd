d163db70b0be758a5a22af6d05b07b6c4c01311b
DaiChiora <111251517+DaiChiora@users.noreply.github.com>
2023-04-21 15:48:29 -0300

    cors habilidades

--- src/main/java/com/ap/ap/controller/HabilidadesController.java
+++ src/main/java/com/ap/ap/controller/HabilidadesController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "https://portfolio-daichiora-front.web.app/")
 @RequestMapping("/habilidades")
 public class HabilidadesController {
     @Autowired
